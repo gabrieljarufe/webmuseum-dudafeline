60820c5298d49dd19b4b12464aa63c0f45cd55dc
Gabriel Jarufe <gabrieljarufe1@gmail.com>
2026-01-18 22:36:00 -0300

    scroll bar functioning

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const TIME_WAIT_ARROW = 1000;
 
-// garante topo absoluto
 window.scrollTo(0, 0);
 
 const intro = document.getElementById('intro');
@@ -9,6 +8,7 @@ const startBtn = document.getElementById('startBtn');
 const stage1 = document.getElementById('stage1');
 const stage2 = document.getElementById('stage2');
 const stage3 = document.getElementById('stage3');
+const stage4 = document.getElementById('stage4');
 
 const caption1 = document.getElementById('caption1');
 const caption2 = document.getElementById('caption2');
@@ -18,13 +18,16 @@ const arrow1 = document.getElementById('arrow1');
 const arrow2 = document.getElementById('arrow2');
 const arrow3 = document.getElementById('arrow3');
 
-// intro
+// Hide stages 2, 3, and 4 initially
+stage2.classList.add('hidden');
+stage3.classList.add('hidden');
+stage4.classList.add('hidden');
+
 window.addEventListener('DOMContentLoaded', () => {
     intro.classList.add('ready');
     setTimeout(() => startBtn.classList.add('show'), 1000);
 });
 
-// entrar na exposição
 startBtn.addEventListener('click', () => {
     window.scrollTo(0, 0);
     intro.classList.add('hide');
@@ -32,14 +35,11 @@ startBtn.addEventListener('click', () => {
     setTimeout(() => {
         intro.style.display = 'none';
         stage1.classList.add('show');
-
     }, 800);
 
     setTimeout(() => caption1.classList.add('show'), 1400);
-    setTimeout(() => arrow1.classList.add('show'), TIME_WAIT_ARROW  );
+    setTimeout(() => arrow1.classList.add('show'), TIME_WAIT_ARROW);
 });
-
-// arrow -> próxima sala
 
 arrow1.addEventListener('click', () => {
     stage2.classList.remove('hidden');
@@ -57,4 +57,3 @@ arrow2.addEventListener('click', () => {
     setTimeout(() => caption3.classList.add('show'), 900);
     setTimeout(() => arrow3.classList.add('show'), TIME_WAIT_ARROW);
 });
-
